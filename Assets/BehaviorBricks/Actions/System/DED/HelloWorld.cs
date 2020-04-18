707c440bd83a31f08bd0b212cea73a23cbc4f308
Tilde <pxn.tilde@protonmail.com>
2020-04-18 01:41:18 -0700

    Added HelloWorld Behavior

---
+++ Assets/BehaviorBricks/Actions/System/DED/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using Pada1.BBCore.Tasks;
+using Pada1.BBCore;
+using System;
+using BBUnity.Actions;
+using UnityEngine;
+
+namespace BBUnity.Actions
+{
+    [Action("DED/HelloWorld")]
+    [Help("Prints a message")]
+    public class HelloWorld : GOAction
+    {
+        [InParam("Message")]
+        [Help("Message to print")]
+        public String Message { get; set; }
+
+        public override TaskStatus OnUpdate()
+        {
+            Debug.Log(Message);
+            return TaskStatus.COMPLETED;
+        }
+    }
+}
