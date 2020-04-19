d3b8e566474955d10b7bc5b8eaa66dd64c4ce4e9
Tilde <pxn.tilde@protonmail.com>
2020-04-18 23:58:55 -0700

    Added swank movement

---
+++ Assets/Scripts/SwankMovementComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwankMovementComponent : MonoBehaviour
+{
+    public Single MoveSpeed = 10f;
+
+    public Vector2 Destination;
+
+    public Single YFactor;
+
+    private void Update()
+    {
+        var position = (Vector2)transform.position;
+        var direction = (Destination - position).normalized;
+        direction.y *= YFactor;
+
+        position += direction * MoveSpeed * Time.deltaTime;
+    }
+}
