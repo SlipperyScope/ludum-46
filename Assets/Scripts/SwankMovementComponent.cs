f0b00c319fef90f7e44325c32a991616e1219580
Tilde <pxn.tilde@protonmail.com>
2020-04-20 14:15:11 -0700

    swankier walk

--- Assets/Scripts/SwankMovementComponent.cs
+++ Assets/Scripts/SwankMovementComponent.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Math;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,18 +8,49 @@ public class SwankMovementComponent : MonoBehaviour
 {
     public Single MoveSpeed = 2f;
 
-    public Single YFactor = 9f/16f;
+    public Single YFactor = 9f / 16f;
 
-    public Vector2 Destination { get; set; }
+    private Vector2 _Destination;
+    public Vector2 Destination
+    {
+        get => _Destination;
+        set
+        {
+            _Destination = value;
+        }
+    }
+
+    public Int32 StepFrames = 7;
+    public  Int32 PauseFrames = 11;
+    public Int32 FramesSinceStartWalk;
 
     public SpriteRenderer SwankySprite;
+
+    public Boolean _IsWalking;
+    public Boolean IsWalking
+    {
+        get => _IsWalking;
+        set
+        {
+            if (value != _IsWalking)
+            {
+                _IsWalking = value;
+                if (IsWalking == true)
+                {
+                    FramesSinceStartWalk = 0;
+                }
+            }
+        }
+    }
 
     private void Start()
     {
         SwankySprite = gameObject.GetComponentInChildren<SpriteRenderer>();
     }
 
-    private void Update()
+    
+
+    private void FixedUpdate()
     {
         var position = (Vector2)transform.position;
         var direction = (Destination - position).normalized;
@@ -29,8 +61,33 @@ public class SwankMovementComponent : MonoBehaviour
             SwankySprite.flipX = direction.x > 0;
         }
 
-        position += direction * MoveSpeed * Time.deltaTime;
-        gameObject.transform.position = position;
+        if (position.DistanceTo(Destination) > 0f)
+        {
+            if (IsWalking == false)
+            {
+                IsWalking = true;
+            }
+
+
+            if (FramesSinceStartWalk > PauseFrames)
+            {
+                position += direction * MoveSpeed * Time.deltaTime;
+                gameObject.transform.position = position;
+            }
+
+            if (FramesSinceStartWalk >= StepFrames + PauseFrames)
+            {
+                FramesSinceStartWalk = 0;
+            } else
+            {
+                FramesSinceStartWalk++;
+            }
+
+        } else
+        {
+            IsWalking = false;
+        }
+
     }
 
     public void StopMoving()
