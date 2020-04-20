0e51dcb55cb92d9437551fb83984186a98041833
Victor <victor@nebulacore.net>
2020-04-19 23:47:09 -0700

    Revert "Revert "Fixed?""
    This reverts commit 361cfd23d6bf1ba965f2384c720df7b5342bd953.

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("Adam");
+    }
+    public void doExitGame()
+    {
+        Application.Quit();
+    }
+}
