e61bc71ad6718ee14237a2a134295930699d35cf
Tilde <pxn.tilde@protonmail.com>
2020-04-20 14:49:58 -0700

    Swank doesn't dance off the dance floor

---
+++ Assets/BehaviorBricks/Conditions/System/DED/OnDancefloor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pada1.BBCore.Framework;
+using Pada1.BBCore;
+using UnityEngine;
+using Math;
+using BBUnity.Conditions;
+
+namespace BBCore.Conditions
+{
+    [Condition("DED/OnDancefloor")]
+public    class OnDancefloor : GOCondition
+    {
+        [InParam("Entity")]
+        public GameObject Entity;
+
+        [InParam("DanceRect")]
+        public Rect DanceRect;
+
+        [InParam("Invert")]
+        public Boolean Invert;
+
+        public override bool Check()
+        {
+            var OnDancefloor = DanceRect.Contains(Entity.transform.position);
+            return Invert ? !OnDancefloor : OnDancefloor;
+        }
+    }
+}
