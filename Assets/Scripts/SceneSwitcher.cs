f4d7110e64c9e67d5809a5519d6740ffb820b8cb
Victor <victor@nebulacore.net>
2020-04-20 17:07:21 -0700

    Added jingle sound effect, slap sound on click, and instructions
    I probably broke things too.

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -3,9 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
     public void GotoMainScene()
     {
         SceneManager.LoadScene("Adam");
+    }
+    public void GotoInstScene()
+    {
+        SceneManager.LoadScene("Inst");
     }
     public void doExitGame()
     {
